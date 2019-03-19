8d048dace83e56947df0bdc7fbb5a178bb091ee6
bogtod <todi.bogdan@gmail.com>
2019-03-19 12:25:47 +0200

    added a tooltip to the go-back-up button and made it appear when the user scrolls to bottom (used JS)

--- Homework/practiceWebSite/js.js
+++ Homework/practiceWebSite/js.js
@@ -1,13 +1,41 @@
 // JS for the scroll up button
-var goUp = document.getElementById('goUp');
-
-window.onscroll = function() {toggleGoUp();};
-
 function toggleGoUp() {
+    var goUpBtn = document.getElementById('goUpBtn');
     if(window.scrollY > 100) {
-        goUp.style = ("display: block;");
+        goUpBtn.style = ("display: block;");
     } else {
-        goUp.style = ("display: none;");
+        goUpBtn.style = ("display: none;");
     }
 };
+
+function goUp() {
+    window.scroll = 0;
+}
+
+
+//JS for bottom scroll tooltip
+function bottomTooltip() {
+    var scrollFind = window.innerHeight + window.pageYOffset;
+    var docHeight = document.body.clientHeight;
+    var tooltip = document.getElementById("tooltip");
+
+    if(scrollFind == docHeight) {
+        tooltip.style = "display: block;";
+        console.log("You reached the bottom of the page!");
+    } else {
+        tooltip.style = "display: none;";
+    }
+}
+
+
+// having the above functions run when the user scrolls
+window.onscroll = function() {
+    toggleGoUp();
+    bottomTooltip();
+};
+
+
+
+
+
 
