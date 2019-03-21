163616734e74df07e24e0bea81e1b5b4f54c1be7
bogtod <todi.bogdan@gmail.com>
2019-03-21 20:48:16 +0200

    added the second part to second inClass practice for JS

--- InClass/JavaScript/secondPractice/js.js
+++ InClass/JavaScript/secondPractice/js.js
@@ -1,3 +1,5 @@
+// PARTEA INTAI
+
 //Ex1
 //Scrieti o functie multiplyByTen care atunci cand o apelam cu console.log(multiplyByTen(6)) sa avem 60 rezultat.
 function multiplyByTen(x) {
@@ -73,3 +75,57 @@ function multiplyByTen(x) {
     var stringResult = degrees + " grade Celsius reprezinta " + result + " grade Farenheit";
     return stringResult;
   }
+
+
+
+  //PARTEA A DOUA
+
+
+  //Ex1 : La ce index intalnim cuvantul 'trece', dar cuvantul 'javascript'? Scrieti si metodele folosite.
+var str = "Daca te simti bine, nu te ingrijora, trece!";
+var result1 = str.indexOf("trece");
+//Raspuns: 
+
+// Ex2.0: Returnati stringul Kiwi folosind metoda slice() cu indexi pozitivi si apoi negativi.
+// Ex 2.1: Returnati toate fructele incepand de la 'Lemon'.
+  var fruits = "Apple, Lemon, Plum, Kiwi";
+var result2 = fruits.slice(20, 24);
+var result2One = fruits.slice(-4);
+var result21 = fruits.slice(7);
+  
+// Ex 3: Returnati stringul 'Plum' folosind metoda substr()
+   var moreFruits = "Apple, Lemon, Plum, Kiwi";
+var moreFruitsResult = moreFruits.substr(14, 19);
+
+// Ex 4: Inlocuiti 'Mia Kalifa' cu numele vostru folosind o metoda pe stringuri
+// bonus: Inlocuiti "Sasha" cu numele vostru in tot stringul de mai jos.
+   var replaceName = "Eu sunt Mia Kalifa, doctor docent in matematica";
+var bogdanRepl = replaceName.replace("Mia Kalifa", "Bogdan");
+   var bonusName = "Sasha este o persoana de nota 10, Sasha invata javascript(mai nou)"
+var bonusRepl = bonusName.replace(/Sasha/g, "Bogdan");
+// Ex5: Vreau ca toate literele sa fie intai cu caractere mici si apoi cu caractere mari
+   var text = "24 de orE intr-o zi, 24 de beRi intr-o lAda. Vreo coincidenta?"
+var textLower = text.toLowerCase();
+   
+// Ex6: Vreau sa am la final un singur string cu toate celalte stringuri (indentati calumea, cu spatii unde e nevoie)
+   var text1 = "Când ai un";
+   var text2 = "ciocan";
+   var text3 = "in mana toate lucrurile ti se par";
+   var text4 = "cuie";
+var concatVar = text1.concat(" ", text2);
+var concatVar = concatVar.concat(" ", text3);
+var concatVar = concatVar.concat(" ", text4);
+
+
+// Ex7: Vreau sa stiu intai ce litera se afla pentru indexul 6, apoi ce unicode are aceasta litera, si apoi vreau sa o accesez cu 'property access'
+  var word = "Mamaliga";
+
+ 
+// Ex8: Vreau sa am o noua variabla care sa cotina un array format din toate stringurile de mai jos care sunt despartite de '|'
+  var cars = "Toyota|Mazda|Tesla|Hyundai|Dacia";
+  
+  
+//Big BONUS 
+// Se dau variabilele de mai jos, vreau sa ajung la rezultatul "NU EXISTA INTREBARI PROASTE" fara sa modific valorile celor 2 variabile;
+  var firstWord = 'Nu exista';
+  var secondWord = 'raspunsuri proaste';
