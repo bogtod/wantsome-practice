dea1e348f820070266df0441dfe8da4dcc98daa1
bogtod <todi.bogdan@gmail.com>
2019-04-17 22:53:44 +0300

    finished exercise from class on DOM manipulation - ex8

--- InClass/JavaScript/Week 9/DOM Manipulation/ex8/js.js
+++ InClass/JavaScript/Week 9/DOM Manipulation/ex8/js.js
@@ -1,15 +1,49 @@
+const commentWrap = document.getElementById('commentWrap')
 const checkbox = document.getElementById('checkbox')
+const commentOutput = document.getElementById('commentOutput')
+const commArea = document.getElementById('userComment')
+const charCount = document.getElementById('charCount')
+const congratsMsg = document.getElementById('successMsg')
+const outputWrap = document.getElementById('commOutputWrap')
+const alert = document.getElementById('alert')
+const btn = document.getElementById('btn')
 
 
+checkbox.onchange = function hideShow() {
+    if(checkbox.checked === true) {
+        commentWrap.style.display = 'flex'
+        congratsMsg.style.display = 'none'
+        commArea.value = ''
+        commArea.focus();
+    } else {
+        commentWrap.style.display = 'none'
+    }
+}
 
-checkbox.onchange = function() {
-    document.querySelector('textarea').classList.toggle('hide')
+commArea.onfocus = function() {
+    commArea.onkeyup = function() {
+        charCount.innerHTML = commArea.value.split('').length
+        if(commArea.value.split('').length > 160) {
+            alert.style.display = 'block'
+            btn.disabled = true
+        } else {
+            alert.style.display = 'none'
+            btn.disabled = false
+        }
+    }
 }
 
 
-
 function checkText() {
-    console.log(document.querySelector('textarea').value)
+    if(commArea.value.split('').length > 160) {
+        
+    } else {
+        commentWrap.style.display = 'none'
+        checkbox.checked = false
+        congratsMsg.style.display = 'block'
+        outputWrap.style.display = 'block'
+        commentOutput.innerHTML = commArea.value
+    }
 }
 
 
