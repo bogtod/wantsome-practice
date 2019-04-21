4bc8d855b8638e610803de82bdc7433d25f6710a
bogtod <todi.bogdan@gmail.com>
2019-04-21 18:58:23 +0300

    added comments for JS

--- Wantsome - JavaScript - Homeworks/JavaScript - Comment Form/js.js
+++ Wantsome - JavaScript - Homeworks/JavaScript - Comment Form/js.js
@@ -1,3 +1,4 @@
+//getting all the needed variables
 const commentWrap = document.getElementById('commentWrap');
 const checkbox = document.getElementById('checkbox');
 const commArea = document.getElementById('userComment');
@@ -10,33 +11,42 @@ const commentOutput = document.getElementById('commentOutput');
 
 
 
-
+//onchange event set on the checkbox
 checkbox.onchange = function() {
     if(checkbox.checked === true) {
+        //if the box is checked, display the comment area and focus on it
         commentWrap.style.display = 'flex';
         commArea.focus();
     } else {
+        //if the box is unchecked, hide the comment area (useful if the user toggles) and disable the submit button
         commentWrap.style.display = 'none';
         btn.disabled = true;
     }
 }
 
-
+//set onfocus event on the comment area
 commArea.onfocus = function() {
+    //set onkeyup event on focus area to check the content everytime the user types inside it
     commArea.onkeyup = function() {
         let count = commArea.value.split('').length;
+
+        //particular cases if 0 chars or 1 character is typed
         if(count === 0) {
+            //if the comment area is empty, also disable the submit button
             btn.disabled = true;
             charCount.textContent = `Your feedback form is empty`;
         } else if(count === 1) {
+            //if the count of chars is 1, enable de button
             btn.disabled = false;
             charCount.textContent = `You entered ${count} character. ${160-count} to go`;
         } else if(count > 1) {
+            //IFs and actions for different char counts
             charCount.textContent = `You entered ${count} characters. ${160-count} to go`;
             if(count >= 150) {
-                charCount.textContent = `You entered ${count} characters. ${160-count} to go. You should thing about stopping now...`;
+                charCount.textContent = `You entered ${count} characters. ${160-count} to go. You should think about stopping now...`;
                 charCount.style.backgroundColor = 'yellow';
                 if(commArea.value.split('').length > 160) {
+                    //if char count is above 160, do the following
                     commArea.style.backgroundColor = 'red';
                     charCount.textContent = `You entered ${count} characters!!!`;
                     charCount.style.color = 'red';
@@ -55,11 +65,15 @@ commArea.onfocus = function() {
     }
 }
 
+
+//function for hitting the submit button
 function sendFeedback() {
     document.querySelector('form').style.display = 'none';
     congratsMsg.style.display = 'block';
     commentOutputWrap.style.display = 'block';
     var now  = new Date();
+
+    //output the user's feedback along with the date and time
     commentOutput.innerHTML = `
         <p><i>${commArea.value}</i></p>
         <hr>
