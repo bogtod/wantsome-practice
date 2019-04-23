2a50448b50d5ebc63e19894034ae779e83dee578
todbog <todi.bogdan@gmail.com>
2019-04-24 00:19:40 +0300

    modified colors json homework

--- Wantsome - JavaScript - Homeworks/colors json/js.js
+++ Wantsome - JavaScript - Homeworks/colors json/js.js
@@ -1,52 +1,62 @@
-const section = document.querySelector('section');
 
-const requestUrl = 'json.json';
-
-const ajaxRequest = function() {
-    const request = new XMLHttpRequest();
-    request.open('GET', requestUrl);
-    request.responseType = 'json';
-    request.send();
-    request.onload = function() {
-        const colors = request.response;
-        console.log(colors);
-        showColors(colors);
-    };
-}
-
-ajaxRequest();
-
-function showColors(jsonObj) {
-    const myList = document.createElement('ul');
-    const colorsKeys = Object.keys(jsonObj);
-    const colorVals = Object.values(jsonObj);
-
-    for(let i = 0; i < colorsKeys.length; i++) {
-        const listItem = document.createElement('li');
-        const colorDiv = document.createElement('div');
-        const textSpan = document.createElement('span');
-
-        colorDiv.classList.add('colorize');
-        textSpan.textContent = colorsKeys[i];
-        colorDiv.style.backgroundColor = colorVals[i];
+function getColors() {
+    const section = document.querySelector('section');
+    const requestUrl = 'json.json';
 
 
-        listItem.appendChild(colorDiv);
-        listItem.appendChild(textSpan);
-        myList.appendChild(listItem);
-        section.appendChild(myList);
+
+    const ajaxRequest = function() {
+        const request = new XMLHttpRequest();
+        request.open('GET', requestUrl);
+        request.responseType = 'json';
+        request.send();
+        request.onload = function() {
+            const colors = request.response;
+            console.log(colors);
+            showColors(colors);
+            section.style.display = 'flex';
+        };
+    }
+
+    ajaxRequest();
+
+    function showColors(jsonObj) {
+        const colorsKeys = Object.keys(jsonObj);
+        const colorVals = Object.values(jsonObj);
+
+        for(let i = 0; i < colorsKeys.length; i++) {
+            //creating elements
+            const wrapDiv = document.createElement('div');
+            const colorDiv = document.createElement('div');
+            const colorName = document.createElement('span');
+            const hexName = document.createElement('span');
+
+            //adding necessary classes
+            colorDiv.classList.add('colorize');
+            colorName.classList.add('colorName');
+            hexName.classList.add('hexName');
+
+            //adding text contents
+            colorName.textContent = colorsKeys[i];
+            hexName.textContent = colorVals[i];
+            
+            //styling
+            wrapDiv.style.border = `1px solid ${colorVals[i]}`;
+            colorDiv.style.backgroundColor = colorVals[i];
+
+            //appending elements
+            wrapDiv.appendChild(hexName);
+            wrapDiv.appendChild(colorDiv);
+            wrapDiv.appendChild(colorName);
+            section.appendChild(wrapDiv);
+
+            //showing and hiding a color's hex name on mouse over
+            wrapDiv.onmouseover = function() {
+                wrapDiv.children[0].style.display = 'inline-block';
+            }
+            wrapDiv.onmouseout = function() {
+                wrapDiv.children[0].style.display = 'none';
+            }
+        }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
