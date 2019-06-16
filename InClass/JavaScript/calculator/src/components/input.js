3b5536da31545c3859e67651702534d2edd404aa
bogtod <todi.bogdan@gmail.com>
2019-06-16 19:50:01 +0300

    performing basic operations

--- InClass/JavaScript/calculator/src/components/input.js
+++ InClass/JavaScript/calculator/src/components/input.js
@@ -5,8 +5,9 @@ class Input extends React.Component{
     render() {
         return (
             <div>
-                <div id="output">{this.props.output}</div>
-                <div id="current">{this.props.current}</div>
+                <div id="output">Output is: {this.props.output}</div>
+                <div id="result">Result is: {this.props.result}</div>
+                <div id="current">Current is: {this.props.current}</div>
             </div>
         )
     }
--- InClass/JavaScript/calculator/src/components/main.js
+++ InClass/JavaScript/calculator/src/components/main.js
@@ -9,32 +9,62 @@ class Main extends React.Component{
         numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9],
         buttons: {
             clearBtn: 'clear',
-            symbols: ['=', '+', '-', '/']
+            symbols: ['=', '+', '-', '/', '*']
         },
         current: 0,
-        output: []
+        input: null,
+        output: [],
+        result: 0,
     };
 
     updateInput = (val, type) => {
-        console.log(type)
+        // console.log(type)
         if(val === 'clear') {
-            this.setState({ current: 0, output: [] })
+            this.setState({ current: 0, result: 0, input: null, output: [] })
         } else if(val === '=') {
-            
-            this.setState({ current: this.state.output })
+            this.setState({ current: 0, input: null, output: [] })
         } else {
-            
-            this.setState({ current: val });
+        
+            // this.setState({ current: val });
             this.setState({ output: [...this.state.output, val] });
-            
+            if(type === 'num') {
+                
+                // this.setState({ result: val });
+
+                if(this.state.input === '+') {
+                    this.setState({ result: Number(this.state.result) + Number(val) })
+                }
+                if(this.state.input === '-') {
+                    this.setState({ result: Number(this.state.result) - Number(val) })
+                }
+                if(this.state.input === '/') {
+                    this.setState({ result: Number(this.state.result) / Number(val) })
+                }
+                if(this.state.input === '*') {
+                    this.setState({ result: Number(this.state.result) * Number(val) })
+                }
+                if(this.state.input === 'num' || this.state.input === null) {
+                    this.setState({ result: `${this.state.result}${val}` });
+                    this.setState({ current: this.state.result });
+                }
+
+
+                this.setState({ input: type });
+ 
+            } else if(type === 'sym') {
+                
+                this.setState({ input: val });
+            }
         }
+        
         
     };
 
     
     render() {
-        console.log(`output is: ${this.state.output.join('')}`);
-        console.log(`current is: ${this.state.current}`)
+        // console.log(`output is: ${this.state.output.join('')}`);
+        // console.log(`current is: ${this.state.current}`)
+        console.log(`Input is: ${this.state.input}`)
         
         let printNums = this.state.numbers.map(item => <Numbers num={item} onClick={this.updateInput} />);
         let printSymbols = this.state.buttons.symbols.map(item => <Symbols symbol={item} onClick={this.updateInput} />)
@@ -43,7 +73,7 @@ class Main extends React.Component{
             <div>
 
                 <div>
-                    <Input output={this.state.output} current={this.state.current} />
+                    <Input result={this.state.result} current={this.state.current} output={this.state.output} />
                 </div>
 
                 <div>
